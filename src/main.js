6fb8c629fca9189a8d87a80065b1e855abf2f4c8
Alexander Nazarenko <olexandr.nazarenko@gmail.com>
2017-05-25 21:04:00 +0300

    added vuex

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
 import router from './router'
 import VeeValidate from 'vee-validate'
 import VueResource from 'vue-resource'
+import store from './store/store'
 
 Vue.config.productionTip = false
 Vue.use(VeeValidate)
@@ -25,6 +26,7 @@ export const eventBus = new Vue({
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App }
 })
---
+++ src/store/modules/task.js
@@ -0,0 +1,76 @@
+const state = {
+  todo: [
+    {
+      id: 1,
+      title: 'Read Books',
+      description: 'fd kjdfb ksfh fkjbds fkjdvb',
+      city: 'Kiev',
+      date: '20.06.2016',
+      weather: {
+        icon: '02d',
+        main: 'Rain',
+        description: 'small rain'
+      }
+    }
+  ],
+  doing: [],
+  done: []
+}
+
+const mutations = {
+  // 'BUY_STOCK'(state, {stockId, quantity, stockPrice}) {
+  //   const record = state.stocks.find(element => element.id === stockId);
+  //   if (record) {
+  //     record.quantity += quantity;
+  //   } else {
+  //     state.stocks.push({
+  //       id: stockId,
+  //       quantity: quantity
+  //     });
+  //   }
+  //   state.funds -= stockPrice * quantity;
+  // },
+  // 'SELL_STOCK'(state, {stockId, quantity, stockPrice}) {
+  //   const record = state.stocks.find(element => element.id === stockId);
+  //   if (record.quantity > quantity) {
+  //     record.quantity -= quantity;
+  //   } else {
+  //     state.stocks.splice(state.stocks.indexOf(record), 1);
+  //   }
+  //   state.funds += stockPrice * quantity;
+  // },
+  // 'SET_PORTFOLIO' (state, portfolio) {
+  //   state.funds = portfolio.funds;
+  //   state.stocks = portfolio.stockPortfolio ? portfolio.stockPortfolio : [];
+  // }
+}
+
+const actions = {
+  // sellStock ({commit}, order) {
+  //   commit('SELL_STOCK', order)
+  // }
+}
+
+const getters = {
+  // stockPortfolio (state, getters) {
+  //   return state.stocks.map(stock => {
+  //     const record = getters.stocks.find(element => element.id === stock.id)
+  //     return {
+  //       id: stock.id,
+  //       quantity: stock.quantity,
+  //       name: record.name,
+  //       price: record.price
+  //     }
+  //   })
+  // },
+  // funds (state) {
+  //   return state.funds
+  // }
+}
+
+export default {
+  state,
+  mutations,
+  actions,
+  getters
+}
